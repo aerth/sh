1c3de2ab5de57eacfdc13c49a35b9e171cbaaa37
aerth <aerth@riseup.net>
2016-05-04 02:59:39 -0700

    This is a commit message.

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 ## For useful scripts
 set -e
+
+if test "$1" = "upgrade"; then
+
+echo Upgrading.
+
+exit 0
+fi
+
+
 SHBIN=$HOME
-echo installing these new tools:
+echo aerth\'s toolbox
+
+echo && echo v99.8 "A new thing"
+echo && echo " . . . . . . . . . . . ."
+echo && echo installing these new tools:
 ls -ACx ./bin ./sbin ./rc.d
 echo $(date) >> uninstall
 echo $(ls -ACx ./bin) >> uninstall
@@ -12,27 +25,43 @@ echo "###" >> uninstall
 echo $(ls -c ./bin)
 echo $(ls -c ./sbin)
 echo $(ls -c ./rc.d)
-sleep 5
-echo 5 4 3 2 1
+echo . . . . . . . . . . . .
+echo Continue? Type yes
+read CONTINUE
+if test $CONTINUE = yes; then
+echo Doing it.
+else
+echo Leaving...
+exit 1
+fi
+
+
+echo mkdir -p $SHBIN/bin || true
 mkdir -p $SHBIN/bin || true
 ## For sudo scripts
+echo mkdir -p $SHBIN/sbin || true
 mkdir -p $SHBIN/sbin || true
 ## For user-level system switches
+echo mkdir -p $SHBIN/rc.d || true
 mkdir -p $SHBIN/rc.d || true
 
 for i in $(ls ./bin); do
+echo ln -s $(pwd)/bin/$i $SHBIN/bin/$i || true
 ln -s $(pwd)/bin/$i $SHBIN/bin/$i || true
 done
 
 for i in $(ls ./sbin); do
+echo ln -s $(pwd)/sbin/$i $SHBIN/sbin/$i || true
 ln -s $(pwd)/sbin/$i $SHBIN/sbin/$i || true
 done
 
 for i in $(ls ./rc.d); do
+echo ln -s $(pwd)/rc.d/$i $SHBIN/rc.d/$i || true
 ln -s $(pwd)/rc.d/$i $SHBIN/rc.d/$i || true
 done;
 
-echo "Installed."
+echo "Installed. You can update this repo with 'git pull origin 
+master' to upgrade, or use '$0 upgrade'."
 echo "Adding to path..."
 echo "PATH=$SHBIN/bin:$PATH" >> ~/.bashrc
 echo "PATH=$SHBIN/bin:$PATH" >> ~/.profile
